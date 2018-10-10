69b79be5c00bfa018f0b57afd90f16ec508f89c6
jdragon.bae <jdragon.bae@gmail.com>
2018-10-11 06:42:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# long-name-cookie
+Test for chromium bug #871639(crbug.com/871639)
+
+<code>
+npm install
+</code>
+</br>
+</br>
+<code>
+node app.js
+</code>
---
+++ app.js
@@ -0,0 +1,19 @@
+var exp = require('express'); 
+var cookieParser = require('cookie-parser'); 
+var app = exp(); 
+ 
+var longNameCookie = ''; 
+ 
+app.use(cookieParser()); 
+app.get('/',function(req,res){
+        console.log('making cookie with long name'); 
+        for(var i=0; i<1000; i++) 
+                longNameCookie+='a'; 
+	res.clearCookie('long_name_cookie');
+        res.cookie('long_cookie',longNameCookie); 
+        res.end('cookie_test');  
+}); 
+ 
+app.listen(3000,function(){ 
+        console.log('Server listening on port 3000'); 
+});
