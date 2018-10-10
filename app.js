94474142fabb13e5fa76963024887d0a2eff07ed
jdragon.bae <jdragon.bae@gmail.com>
2018-10-11 06:55:17 +0900

    Fix code indentation style

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var longNameCookie = '';
  
 app.use(cookieParser()); 
 app.get('/',function(req,res){
-        console.log('making cookie with long name'); 
-        for(var i=0; i<1000; i++) 
-                longNameCookie+='a'; 
+	console.log('making cookie with long name'); 
+	for(var i=0; i<1000; i++) 
+		longNameCookie+='a'; 
 	res.clearCookie('long_name_cookie');
-        res.cookie('long_cookie',longNameCookie); 
-        res.end('cookie_test');  
+	res.cookie('long_cookie',longNameCookie); 
+	res.end('cookie_test');  
 }); 
  
 app.listen(3000,function(){ 
-        console.log('Server listening on port 3000'); 
+	console.log('Server listening on port 3000'); 
 });
